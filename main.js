30aa9ff3d9dd37aefcbe270c8790b7409fc501bc
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2025-04-22 12:27:58 -0300

    adding js and sketch files

---
+++ main.js
@@ -0,0 +1,8 @@
+// function past(h, m, s){
+//   // Multiple the variable by milliseconds and add them together
+//   return (h * 3600000) + (m * 60000) + (s * 1000);
+  
+// }
+
+// Multiple the variable by milliseconds and add them together
+past = (h, m, s) => (h * 3600000) + (m * 60000) + (s * 1000);
